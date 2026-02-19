66da7706d978c65c49377091c9fd1e6dcd63bf90
gregorysylvestrent-droid <gregory.sylvestrent@gmail.com>
2026-02-19 10:34:58 -0400

    Switch package manager from dnf to apt

--- infra/aws/bootstrap-ec2.sh
+++ infra/aws/bootstrap-ec2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-dnf update -y
-dnf install -y git nginx rsync
-curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
-dnf install -y nodejs
+apt update -y
+apt install -y git nginx rsync
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y nodejs
 npm install -g pm2
 
 mkdir -p /var/www/logiwms
